c2bba849ffaaef6e7160c86f6d4965e67548bd5f
William Olei <seanapowi@gmail.com>
2024-10-04 04:54:35 +0300

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { Container } from 'reactstrap';
 import Home from './Components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Registration from './Components/Registration';
+import Login from './Components/Login';
+import Logout from './Components/Logout';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/registration" element={<Registration />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
--- frontend/src/Components/Home/index.jsx
+++ frontend/src/Components/Home/index.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import CustomNav from '../CustomNav';
 import { userData } from '../../../helpers';
 
